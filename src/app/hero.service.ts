951370c31676eba22eb476b7c67fb963d9301e90
jun9130 <jun.nakama1030@gmail.com>
2021-08-13 18:02:59 +0900

    HeroService への注入

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,9 +3,11 @@ import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 
-
 //サーバからのレスポンス 非同期処理
 import { Observable, of } from 'rxjs';
+
+//MessageServiceを注入
+import { MessageService } from './message.service';
 
 
 @Injectable({
@@ -13,7 +15,9 @@ import { Observable, of } from 'rxjs';
 })
 export class HeroService {
 
-  constructor() { }
+  constructor(
+    private messageService: MessageService
+  ) { }
 
   // getHeroes(): Hero[] {
   //   return HEROES;
