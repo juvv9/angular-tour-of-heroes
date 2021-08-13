c318d9ce7108b1f78cd15fbe1200916adf4361d2
jun9130 <jun.nakama1030@gmail.com>
2021-08-13 17:34:41 +0900

    Observable HeroService

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,6 +3,11 @@ import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 
+
+//サーバからのレスポンス 非同期処理
+import { Observable, of } from 'rxjs';
+
+
 @Injectable({
   providedIn: 'root'
 })
@@ -10,7 +15,13 @@ export class HeroService {
 
   constructor() { }
 
-  getHeroes(): Hero[] {
-    return HEROES;
+  // getHeroes(): Hero[] {
+  //   return HEROES;
+  // }
+
+  //サーバからのレスポンス 非同期処理
+  getHeroes(): Observable<Hero[]> {
+    const heroes = of(HEROES);
+    return heroes;
   }
 }
