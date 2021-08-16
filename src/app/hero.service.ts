6af19feec7ac7dfaff38b166c8a5c8972d16e377
jun9130 <jun.nakama1030@gmail.com>
2021-08-16 14:25:40 +0900

    handleError

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -66,5 +66,25 @@ export class HeroService {
 
   private heroesUrl = 'api/heroes';  // Web APIのURL
 
+  /**
+ * 失敗したHttp操作を処理します。
+ * アプリを持続させます。
+ * @param operation - 失敗した操作の名前
+ * @param result - observableな結果として返す任意の値
+ */
+private handleError<T>(operation = 'operation', result?: T) {
+  return (error: any): Observable<T> => {
+
+    // TODO: リモート上のロギング基盤にエラーを送信する
+    console.error(error); // かわりにconsoleに出力
+
+    // TODO: ユーザーへの開示のためにエラーの変換処理を改善する
+    this.log(`${operation} failed: ${error.message}`);
+
+    // 空の結果を返して、アプリを持続可能にする
+    return of(result as T);
+  };
+}
+
 
 }
