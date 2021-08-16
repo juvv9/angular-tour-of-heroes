aee1c587dfd62a687f0174d4094053b604a57d53
jun9130 <jun.nakama1030@gmail.com>
2021-08-16 13:32:10 +0900

    HeroService.getHero()を追加する

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -35,4 +35,12 @@ export class HeroService {
     this.messageService.add('HeroService: fetched heroes 文字列'); //messageService呼び出し
     return heroes;
   }
+
+  getHero(id: number): Observable<Hero> {
+    // For now, assume that a hero with the specified `id` always exists.
+    // Error handling will be added in the next step of the tutorial.
+    const hero = HEROES.find(h => h.id === id)!;
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(hero);
+  }
 }
