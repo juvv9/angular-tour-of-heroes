b85094c6994c536600db575ebb73b27cbb0464aa
jun9130 <jun.nakama1030@gmail.com>
2022-05-15 22:54:11 +0900

    lesson

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,13 @@
 import { NgModule } from '@angular/core';
-// import { CommonModule } from '@angular/common';
-
 import { RouterModule, Routes } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
 
 const routes: Routes = [
-  //ルートパス
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-]
-
-
+  { path: 'heroes', component: HeroesComponent }
+];
 
 @NgModule({
-  declarations: [],
-  imports: [ RouterModule.forRoot(routes) ],
-  exports: [ RouterModule ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
