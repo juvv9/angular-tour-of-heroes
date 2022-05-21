245de63479fdf5003c5b365a9759324403d2cfd6
jun9130 <jun.nakama1030@gmail.com>
2022-05-21 13:33:07 +0900

    ヒーローエディタの編集

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
+import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
   ],
   declarations: [
     AppComponent,
+    HeroesComponent,
   ],
   bootstrap: [ AppComponent ]
 })
