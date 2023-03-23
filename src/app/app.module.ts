c7e974ad0401d8bbb0ad9788eb867461cc96ee16
jun9130 <jun.nakama1030@gmail.com>
2023-03-24 00:09:27 +0900

    プロジェクトの作成

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,23 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { AppRoutingModule } from './app-routing.module';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    FormsModule,
-    AppRoutingModule,
-    HttpClientModule,
-  ],
   declarations: [
-
+    AppComponent
   ],
-  bootstrap: [ ]
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
